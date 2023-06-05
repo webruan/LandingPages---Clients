1766949bd7c31551ade37da62a9a3620a811a522
Ruan Augusto Bernardi Pasa <principalruanbernardi@gmail.com>
2023-06-05 12:21:53 -0300

    Update (FB Pixel)
    Facebook pixel update not tracking events

--- Victour Viagens/js/components.js
+++ Victour Viagens/js/components.js
@@ -1,36 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     components = 
     `
-    <!-- Meta Pixel Code -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '4355715007878986');
-        fbq('track', 'PageView');
-    </script>
-
-    <noscript>
-        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=4355715007878986&ev=PageView&noscript=1"/>
-    </noscript>
-    <!-- End Meta Pixel Code -->
-
-    <script>
-        window.fbAsyncInit = function() {
-            FB.init({
-            appId            : '209649161704696',
-            autoLogAppEvents : true,
-            xfbml            : true,
-            version          : 'v16.0'
-            });
-        };
-    </script>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+    
 
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' async>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
